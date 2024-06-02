5359e716b9a3a3b81b8c02fb941924d56a7dece4
sameer gupta <guptasameer753@gmsil.com>
2024-06-02 23:34:42 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,81 @@
+let boxes = document.querySelectorAll(".box");
+let resetbtn = document.querySelector("#reset-btn");
+let newGameBtn = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
+
+let turn0 = true ; //playerx or player 0
+
+const winPatterns = [
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [3,4,5],
+    [6,7,8],
+];
+
+const resetGame = () => {
+    turn0 = true;
+    enableBoxes();
+    msgContainer.classList.add("hide");
+};
+
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        console.log("box was clicked");
+       
+
+        if (turn0){
+           box.innerText= "O";
+           turn0=false; //player0 
+        }else{
+            box.innerText="X";
+            turn0=true; //player1
+        }
+        box.disabled = true;
+        checkWinner();
+    });
+});
+
+const disableBoxes = () => {
+    for(let box of boxes){
+        box.disabled = true;
+    }
+};
+
+const enableBoxes = () => {
+    for(let box of boxes){
+        box.disabled = false;
+        box.innerHTML = "";
+    }
+};
+
+
+const showWinner = (winner) => {
+    msg.innerText = `Congratulations , Winner is ${winner}`;
+    msgContainer.classList.remove("hide");
+    disableBoxes();
+}
+
+const checkWinner = () => {
+    for(let pattern of winPatterns){
+        let pos1Val = boxes[pattern[0]].innerText;
+        let pos2Val = boxes[pattern[1]].innerText;
+        let pos3Val = boxes[pattern[2]].innerText;
+
+        if (pos1Val != "" && pos2Val != "" && pos3Val != ""){
+            if(pos1Val === pos2Val && pos2Val === pos3Val){
+                console.log("winner" , pos1Val);
+
+                showWinner(pos1Val);
+            }
+        }
+    }
+};
+
+
+newGameBtn.addEventListener("click", resetGame);
+resetbtn.addEventListener("click", resetGame);
